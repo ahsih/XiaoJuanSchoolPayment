71c072340f81c8ebb58eba2a7261109881b96960
Ahsih <ahsih1zhang@hotmail.com>
2025-11-16 21:50:17 +0000

    Updated school service
    Updated school service

--- XiaoJuanSchoolPayment.Server/Services/School/SchoolService.cs
+++ XiaoJuanSchoolPayment.Server/Services/School/SchoolService.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using XiaoJuanSchoolPayment.Server.Data;
 using XiaoJuanSchoolPayment.Server.Data.DTO;
+using XiaoJuanSchoolPayment.Server.Data.Filter;
 using XiaoJuanSchoolPayment.Server.Interface;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace XiaoJuanSchoolPayment.Server.Services.School
 {
@@ -29,20 +31,6 @@ namespace XiaoJuanSchoolPayment.Server.Services.School
       }
       await _appDbContext.SaveChangesAsync();
       return true;
-    }
-
-    public async Task<IList<SchoolDTO>> GetSchools(CancellationToken cancellationToken)
-    {
-      var result = await _appDbContext.Schools
-      .AsNoTracking()
-      .Select(x => new SchoolDTO
-      {
-        Id = x.Id,
-        CreatedDate = x.CreatedDate,
-        Name = x.Name
-      })
-      .ToListAsync(cancellationToken);
-      return result;
     }
 
     public async Task<bool> SaveSchoolLesson(SchoolLessonDTO lesson, CancellationToken cancellationToken)
@@ -171,9 +159,30 @@ namespace XiaoJuanSchoolPayment.Server.Services.School
       return true;
     }
 
-    public async Task<IList<SchoolLessonDTO>> GetSchoolLessons(CancellationToken ct)
+    public async Task<IList<SchoolDTO>> GetSchools(SchoolFilter filter,CancellationToken cancellationToken)
     {
-      var result = await _appDbContext.SchoolLessons.AsNoTracking()
+      var query = _appDbContext.Schools
+      .AsNoTracking()
+      .Select(x => new SchoolDTO
+      {
+        Id = x.Id,
+        CreatedDate = x.CreatedDate,
+        Name = x.Name
+      })
+      .AsQueryable();
+
+      if (filter?.Name != null)
+      {
+        query = query.Where(x => x.Name.ToLower().Contains(filter.Name.ToLower()));
+      }
+
+      var result = await query.ToListAsync(cancellationToken);
+      return result;
+    }
+
+    public async Task<IList<SchoolLessonDTO>> GetSchoolLessons(LessonFilter filter,CancellationToken ct)
+    {
+      var query = _appDbContext.SchoolLessons.AsNoTracking()
         .Include(x => x.School)
         .Include(x => x.Currency)
         .Select(x => new SchoolLessonDTO
@@ -188,13 +197,41 @@ namespace XiaoJuanSchoolPayment.Server.Services.School
           Note = x.Note,
           Price = x.Price,
           Week = x.Week,
-        }).ToListAsync(ct);
+        }).AsQueryable();
+
+      if(filter?.SchoolId != null)
+      {
+        query = query.Where(x => x.SchoolId == filter.SchoolId);
+      }
+
+      if (filter?.MinPrice != null)
+      {
+        query = query.Where(x => filter.MinPrice <= filter.MinPrice);
+      }
+
+      if (filter?.MaxPrice != null)
+      {
+        query = query.Where(x => x.Price <=  filter.MaxPrice);
+      }
+
+      if (filter?.Name != null)
+      {
+        query = query.Where(x => x.Name.ToLower().Contains(filter.Name.ToLower()));
+      }
+
+      if (filter?.Week != null)
+      { 
+        query = query.Where(x => x.Week != filter.Week);
+      }
+
+      var result = await query.ToListAsync(ct);
+
       return result;
     }
 
-    public async Task<IList<SchoolRoomDTO>> GetSchoolRooms(CancellationToken cancellationToken)
+    public async Task<IList<SchoolRoomDTO>> GetSchoolRooms(SchoolRoomFilter filter,CancellationToken cancellationToken)
     {
-      var result = await _appDbContext.SchoolRooms.AsNoTracking()
+      var query = _appDbContext.SchoolRooms.AsNoTracking()
         .Include(x => x.School)
         .Include(x => x.Currency)
         .Select(x => new SchoolRoomDTO
@@ -208,13 +245,31 @@ namespace XiaoJuanSchoolPayment.Server.Services.School
           SchoolId = x.SchoolId,
           Week = x.Week,
           SchoolName = x.School != null ? x.School.Name : "",
-        }).ToListAsync(cancellationToken);
+        }).AsQueryable();
+
+      if(filter?.Name != null)
+      {
+        query = query.Where(x => x.Name.ToLower().Contains(filter.Name.ToLower()));
+      }
+
+      if (filter?.SchoolId != null)
+      {
+        query = query.Where(x => x.SchoolId != filter.SchoolId);
+      }
+
+      if (filter?.Week != null)
+      { 
+        query = query.Where(x => x.Week != filter.Week);
+      }
+
+      var result = await query.ToListAsync(cancellationToken);
+
       return result;
     }
 
-    public async Task<IList<SchoolFeeDTO>> GetSchoolFees(CancellationToken cancellationToken)
+    public async Task<IList<SchoolFeeDTO>> GetSchoolFees(SchoolFeeFilter filter,CancellationToken cancellationToken)
     {
-      var result = await _appDbContext.SchoolFees.AsNoTracking()
+      var query = _appDbContext.SchoolFees.AsNoTracking()
         .Include(x => x.School)
         .Include(x => x.Currency)
         .Select(x => new SchoolFeeDTO
@@ -227,13 +282,36 @@ namespace XiaoJuanSchoolPayment.Server.Services.School
           Fee = x.Fee,
           SchoolId = x.SchoolId,
           SchoolName = x.School != null ? x.School.Name : "",
-        }).ToListAsync(cancellationToken);
+        }).AsQueryable();
+
+      if(filter?.Name != null) 
+      {
+        query = query.Where(x => x.Name.ToLower().Contains(filter.Name.ToLower()));
+      }
+
+      if(filter?.MinFee  != null)
+      {
+        query = query.Where(x => filter.MinFee <= x.Fee);
+      }
+
+      if (filter?.MaxFee != null)
+      {
+        query = query.Where(x => x.Fee <= filter.MaxFee);
+      }
+
+      if (filter?.SchoolId != null)
+      {
+        query = query.Where(x => x.SchoolId == filter.SchoolId);
+      }
+
+      var result = await query.ToListAsync(cancellationToken);
+
       return result;
     }
 
-    public async Task<IList<SchoolNoteDTO>> GetSchoolNotes(CancellationToken cancellationToken)
+    public async Task<IList<SchoolNoteDTO>> GetSchoolNotes(SchoolNoteFilter filter,CancellationToken cancellationToken)
     {
-      var result = await _appDbContext.SchoolNotes.AsNoTracking()
+      var query = _appDbContext.SchoolNotes.AsNoTracking()
         .Include(x => x.School)
         .Select(x => new SchoolNoteDTO
         {
@@ -241,7 +319,21 @@ namespace XiaoJuanSchoolPayment.Server.Services.School
           Notes = x.Notes,
           SchoolId = x.SchoolId,
           SchoolName = x.School != null ? x.School.Name : "",
-        }).ToListAsync(cancellationToken);
+        }).AsQueryable();
+
+
+      if (filter?.SchoolId != null)
+      {
+        query = query.Where(x => x.SchoolId == filter.SchoolId);
+      }
+
+      if (filter?.Note != null)
+      {
+        query = query.Where(x => x.Notes.ToLower().Contains(filter.Note.ToLower()));
+      }
+
+      var result = await query.ToListAsync(cancellationToken);
+
       return result;
     }
   }
