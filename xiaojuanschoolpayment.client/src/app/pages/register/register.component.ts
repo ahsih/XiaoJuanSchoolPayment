6ac33ee5a1acb6a2cc4d10cb544114ade3fcd8b8
Ahsih <ahsih1zhang@hotmail.com>
2026-09-04 22:51:59 +0100

    Reenable register

--- xiaojuanschoolpayment.client/src/app/pages/register/register.component.ts
+++ xiaojuanschoolpayment.client/src/app/pages/register/register.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
-import { Location } from '@angular/common';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { HttpClient } from '@angular/common/http';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { AuthService } from '../../../services/auth.service';
 import { SchoolUserDTO } from '../../../interfaces/SchoolUser.dto';
@@ -15,17 +13,13 @@ import { TranslationService } from '../../../services/translation.service';
 })
 export class RegisterComponent {
   registerForm: FormGroup;
-  successMessage = '';
-  errorMessage = '';
   hidePassword = true;
   loading = false;
 
   constructor(private fb: FormBuilder,
-    private http: HttpClient,
     private snackBar: MatSnackBar,
     private authService: AuthService,
-    private translationService: TranslationService,
-    private location: Location
+    private translationService: TranslationService
   ) {
     this.registerForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
@@ -36,36 +30,35 @@ export class RegisterComponent {
     });
   }
 
-  goBack(): void {
-    this.location.back();
-  }
-
-  onSubmit() {
-    this.loading = true;
-    if (this.registerForm.valid) {
-      const user: SchoolUserDTO = this.registerForm.value;
-
-      this.authService.register(user).subscribe({
-        next: () => {
-          this.snackBar.open(
-            this.translationService.instant('register.success'),
-            this.translationService.instant('common.close'),
-            { duration: 3000 }
-          );
-          this.registerForm.reset();
-          this.loading = false;
-        },
-        error: (err) => {
-          const msg =
-            typeof err.error === 'string'
-              ? err.error
-              : this.translationService.instant('register.failed');
-          this.snackBar.open(msg, this.translationService.instant('common.close'), {
-            duration: 5000,
-          });
-          this.loading = false;
-        }
-      });
+  onSubmit(): void {
+    if (this.registerForm.invalid || this.loading) {
+      this.registerForm.markAllAsTouched();
+      return;
     }
+
+    this.loading = true;
+    const user: SchoolUserDTO = this.registerForm.getRawValue();
+
+    this.authService.register(user).subscribe({
+      next: () => {
+        this.snackBar.open(
+          this.translationService.instant('register.success'),
+          this.translationService.instant('common.close'),
+          { duration: 3000 }
+        );
+        this.registerForm.reset();
+        this.loading = false;
+      },
+      error: (err) => {
+        const msg =
+          typeof err.error === 'string'
+            ? err.error
+            : this.translationService.instant('register.failed');
+        this.snackBar.open(msg, this.translationService.instant('common.close'), {
+          duration: 5000,
+        });
+        this.loading = false;
+      }
+    });
   }
 }
